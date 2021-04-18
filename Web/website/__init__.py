475025b89ef1f6f32f4615c96fed90794fb52177
unknown <lsjayawardana95@gmail.com>
2021-04-18 23:47:53 +0530

    Web Api initial added

---
+++ Web/website/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+from os import path
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+    return app
